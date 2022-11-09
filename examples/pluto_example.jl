3d73eb96270bb57260c73ede8d8a2c9c37dd5a74
roflmaostc <fxw+git@mailbox.org>
2022-11-09 16:49:31 +0100

    Update examples

--- examples/pluto_example.jl
+++ examples/pluto_example.jl
@@ -21,7 +21,7 @@ using Pkg
 Pkg.activate(".")
 
 # ╔═╡ 58407289-ce99-4e8e-a52b-2dee09c225a0
-using Revise, SimpleImageView, Colors, PlutoUI
+using Revise, SimpleImageView, Colors, PlutoUI, FFTW
 
 # ╔═╡ f81c408a-9677-4c3d-9a14-b0bca324f8f9
 using TestImages
@@ -42,10 +42,12 @@ md"## This works already with Colors"
 Gray.(arr)
 
 # ╔═╡ 4d7837d8-0a34-4cfc-a8b0-e7fa9dd93e02
-md"## Let's pimp it with `simshow`"
+md"## Let's pimp it with `simshow`
+Look at the help with `?simshow`
+"
 
 # ╔═╡ 6ed33866-d441-4abb-96a4-39862d94d03c
-?simshow
+simshow
 
 # ╔═╡ 60964d5d-90df-46c6-8bd8-c2e92cec2204
 simshow(img)
@@ -71,9 +73,6 @@ md"# Pass a function"
 # ╔═╡ 4ba38e5e-1d80-4d57-afb4-e281c1746643
 simshow(arr,  f = x -> log.(0.5 .+ x))
 
-# ╔═╡ 52d610c0-8612-4c12-8480-a662f1e58232
-
-
 # ╔═╡ 00a9d5d5-8353-4e91-962a-2a0068696300
 md"## Complex numbers!
 
@@ -89,26 +88,11 @@ md"absγ = $(@bind absγ Slider(0.1:0.1:4, show_value=true))"
 # ╔═╡ f9bcc245-0691-49f7-8f2e-fd6cda4f9adf
 simshow(arr .* exp.(1im .* 2π .* arr2); absγ)
 
-# ╔═╡ 6966f90e-f715-4c1e-b6cc-251d8a775269
+# ╔═╡ a2d97aab-78c3-40f7-8db1-14b5f4004023
+md"## Apply a function to the complex valued array"
 
-
-# ╔═╡ 1f9e1bfa-0143-40d1-b9e6-8a5e8ec5880a
-
-
-# ╔═╡ e1ea3e79-1814-449a-a8f6-e076e07beff9
-
-
-# ╔═╡ a243b2eb-65aa-43f2-bfab-e0334b0dda39
-@show simshow([1.0 / 2, 1im / 2, -1 / 2, -1im / 2, 1.0 - 0.0001im], absγ=2)
-
-# ╔═╡ 363d7de0-cbca-4c37-9c81-00252b78451f
-@show simshow([1.0, 1im / 4, -1 / 8, -1im / 2, 1.0 - 0.0001im], absf = (x -> (y = copy(x); y .= x[1]; y)))
-
-# ╔═╡ ed526a97-0c64-4adc-b719-d8fce73dff9a
-(x -> (y = copy(x); y .= x[1]; y))(randn((2,2)))
-
-# ╔═╡ 67d0ae26-5478-4039-9a0c-10fed40e58f0
-@show simshow(ColorTypes.Gray.([0.123]))
+# ╔═╡ 28238d7c-c505-432a-b901-3bb411ed0b62
+simshow(arr .* exp.(1im .* 2π .* arr2); f=x -> fftshift(fft(x)), absγ=0.1)
 
 # ╔═╡ Cell order:
 # ╠═6b75cf1a-603b-11ed-37ee-d7f2b17aadb4
@@ -120,7 +104,7 @@ simshow(arr .* exp.(1im .* 2π .* arr2); absγ)
 # ╠═7f8e71ad-9df5-4d64-b981-bfd527ea4c4b
 # ╟─b3ac88ae-537c-4d4b-9d05-265920cbe5fa
 # ╠═3653f52f-056b-4fae-8962-992643970f34
-# ╟─4d7837d8-0a34-4cfc-a8b0-e7fa9dd93e02
+# ╠═4d7837d8-0a34-4cfc-a8b0-e7fa9dd93e02
 # ╠═6ed33866-d441-4abb-96a4-39862d94d03c
 # ╠═60964d5d-90df-46c6-8bd8-c2e92cec2204
 # ╟─18952447-7a83-4551-b033-09cffc7487ea
@@ -130,15 +114,9 @@ simshow(arr .* exp.(1im .* 2π .* arr2); absγ)
 # ╠═951cc324-e63e-4122-ab94-250fad83afe6
 # ╟─4c358769-2e61-4463-97de-7cfa8a0e605e
 # ╠═4ba38e5e-1d80-4d57-afb4-e281c1746643
-# ╠═52d610c0-8612-4c12-8480-a662f1e58232
 # ╟─00a9d5d5-8353-4e91-962a-2a0068696300
 # ╠═703e2573-e0ca-4730-8746-4c07bcc8cb80
 # ╠═f9bcc245-0691-49f7-8f2e-fd6cda4f9adf
 # ╠═270fe924-6cb0-4571-a537-9b262f69651b
-# ╠═6966f90e-f715-4c1e-b6cc-251d8a775269
-# ╠═1f9e1bfa-0143-40d1-b9e6-8a5e8ec5880a
-# ╠═e1ea3e79-1814-449a-a8f6-e076e07beff9
-# ╠═a243b2eb-65aa-43f2-bfab-e0334b0dda39
-# ╠═363d7de0-cbca-4c37-9c81-00252b78451f
-# ╠═ed526a97-0c64-4adc-b719-d8fce73dff9a
-# ╠═67d0ae26-5478-4039-9a0c-10fed40e58f0
+# ╟─a2d97aab-78c3-40f7-8db1-14b5f4004023
+# ╠═28238d7c-c505-432a-b901-3bb411ed0b62
